357dcccbbe01f489c42e52afd3091dacd4d43a1a
Syed Abdul Munim Ali Shah <syedmuuni@gmail.com>
2024-05-09 01:52:24 +0500

    save changes

---
+++ DropDown/DropDown/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DropDown.Models;
+
+public partial class Employee
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string ConformPassword { get; set; } = null!;
+}
